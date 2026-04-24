6c08c102d1a51f39154f54a46cbab53c8823b6fd
goddns777 <152259531+goddns777@users.noreply.github.com>
2026-04-24 11:43:56 +0900

    Update 1-3.c

--- code/1-3.c
+++ code/1-3.c
@@ -2,7 +2,7 @@
 
 char *my_strcat(char *dst, const char *src) {
     char *ptr = dst;
-    
+
     while (*ptr != '\0') {
         ptr++;
     }
@@ -12,15 +12,12 @@ char *my_strcat(char *dst, const char *src) {
         src++;
     }
     *ptr = '\0';
-
     return dst;
 }
 
-
-int main(){
+int main() {
     char str1[20] = "Hello";
     char str2[] = "World";
-
     printf("%s\n", str1);
     my_strcat(str1, str2);
     printf("%s\n", str1);
